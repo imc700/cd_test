77e2aaa83af3a704be62e7f99672f484a77dae72
hanxu30 <hanxu30@midea.com>
2021-09-28 14:52:58 +0800

    Initial commit

---
+++ src/main/java/com/example/cd_test/CdTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.cd_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CdTestApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/cd_test/index.java
@@ -0,0 +1,12 @@
+package com.example.cd_test;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class index {
+    @RequestMapping("/h")
+    public String hello() {
+        return "111";
+    }
+}
