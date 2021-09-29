e930f0e7a400add0dfd5273f25e52e0f54066b31
hanxu30 <hanxu30@midea.com>
2021-09-29 10:07:29 +0800

    cd脚本编写10

--- Start.sh
+++ Start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #这里使用的前提是通过ssh设置免密登录
+scp ./Deploy.sh root@116.63.138.138:/hx/cd_test_01.sh
 ssh -tt root@116.63.138.138 << eeooff
-sh Deploy.sh
+sh /hx/cd_test_01.sh
 exit
 eeooff
 echo done
